df3005babca98cec2b88a3dd1898c7521e3eab79
lime <lime>
2021-12-07 21:31:08 +0300

    fix fnm directory

--- deb/fnm.sh
+++ deb/fnm.sh
@@ -5,7 +5,7 @@ if [[ -n `command -v fnm` ]]; then
 else
 	curl -o /tmp/fnm "https://fnm.vercel.app/install"
   chmod +x /tmp/fnm
-  tmp/fnm --install-dir "$HOME/.fnm" --skip-shell
+  /tmp/fnm --install-dir "$HOME/.fnm" --skip-shell
   exec bash -l
   fnm install "v17.1.0"
   fnm use "v17.1.0"
