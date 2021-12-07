b69733e87242d95b1666b8ffd592a217e2bc64d2
lime <lime>
2021-12-07 21:50:55 +0300

    fix go installer

--- deb/go.sh
+++ deb/go.sh
@@ -3,6 +3,6 @@
 if [[ -n `command -v go` ]]; then
   echo Go already installed, skipping...
 else
-  curl -o /tmp/go.tar.gz "https://go.dev/dl/go1.17.4.linux-amd64.tar.gz"
-  tar xzv --directory="$HOME" -f /tmp/go.tar.gz
+  curl -Lo "/tmp/go.tar.gz" "https://go.dev/dl/go1.17.4.linux-amd64.tar.gz"
+  tar xzv --directory="$HOME" -f "/tmp/go.tar.gz"
 fi
