0144828afd09ffdc7e0bd79acbe3e6a53b87e0e8
lime <lime>
2021-12-07 21:42:37 +0300

    add go installer

---
+++ deb/go.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ -n `command -v go` ]]; then
+  echo Go already installed, skipping...
+else
+  curl -o /tmp/go.tar.gz "https://go.dev/dl/go1.17.4.linux-amd64.tar.gz"
+  tar xzv --directory="$HOME" -f /tmp/go.tar.gz
+fi
