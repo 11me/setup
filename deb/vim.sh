be22506310321f526d78a7f8e267121099d2e0d9
lime <lime>
2021-12-07 08:58:49 +0300

    Add vim installer

---
+++ deb/vim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ -n `command -v vim` ]]; then
+  echo Vim already installed, skipping...
+else
+  sudo apt update -y && \
+  sudo apt install -y \
+                git \
+                libxt-dev \
+                libncurses-dev \
+                gcc \
+                make \
+                clang \
+                libtool-bin
+
+  [[ ! -d $PWD/dwns ]] && mkdir -p $PWD/dwns
+  git clone git@github.com:vim/vim.git $PWD/dwns/vim
+  cd $PWD/dwns/vim/src && reconfigure && sudo make
+fi
