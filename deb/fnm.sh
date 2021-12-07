6baae467b9bd16fa2af96314bfc586da7536c054
lime <lime>
2021-12-07 21:29:30 +0300

    fnm fix

--- deb/fnm.sh
+++ deb/fnm.sh
@@ -3,7 +3,10 @@
 if [[ -n `command -v fnm` ]]; then
   echo Fnm already installed, skipping...
 else
-	curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "./.fnm" --skip-shell
+	curl -o /tmp/fnm "https://fnm.vercel.app/install"
+  chmod +x /tmp/fnm
+  tmp/fnm --install-dir "$HOME/.fnm" --skip-shell
+  exec bash -l
   fnm install "v17.1.0"
   fnm use "v17.1.0"
 fi
