24498e67dc418e3597846eb10623ed294f9acf20
lime <lime>
2021-12-07 21:09:45 +0300

    add node to fnm

--- deb/fnm.sh
+++ deb/fnm.sh
@@ -4,4 +4,6 @@ if [[ -n `command -v fnm` ]]; then
   echo Fnm already installed, skipping...
 else
 	curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "./.fnm" --skip-shell
+  fnm install "v17.1.0"
+  fnm use "v17.1.0"
 fi
