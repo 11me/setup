a829836f7fd39ade27ba088f84d6f2c2f57ec741
lime <lime>
2021-12-07 17:52:14 +0300

    add dotfiles option

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ Usage:
 Options:
   --deb  - install for debian based distributions
   --arch - install for archlinux based distributions
+  --dot  - install dotfiles
 DOC
 }
 
@@ -28,11 +29,39 @@ execute() {
   fi
 }
 
+create_link() {
+  echo "Linking: $1 -> $2" && ln -sf $1 $2
+}
+
+install_dot() {
+  dotfiles="$HOME/dotfiles"
+
+  [[ -z `command -v git` ]] && \
+    echo "INFO: git not found." && \
+    sudo apt update -y && \
+    sudo apt install -y git >/dev/null
+
+  if [[ ! -d $dotfiles ]]; then
+
+    git clone git@github.com:11me/dotfiles.git "$dotfiles"
+
+  fi
+
+  create_link "$dotfiles/.config/tmux/.tmux.conf" "$HOME/.tmux.conf"
+  create_link "$dotfiles/.config/vim" "$HOME/.vim"
+  create_link "$dotfiles/.local/bin" "$HOME/.local/bin"
+  create_link "$dotfiles/.bashrc" "$HOME/.bashrc"
+  create_link "$dotfiles/.dircolors" "$HOME/.dircolors"
+
+}
+
 case "$1" in
   --deb)
       execute "deb";;
   --arch)
       echo arch;;
+  --dot)
+      install_dot;;
   *)
-    help
+      help;;
 esac
