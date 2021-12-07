d4094cf2ca210cbdc850d43b66d6ffce0105afa4
lime <lime>
2021-12-07 21:33:35 +0300

    minor fix to fnm

--- deb/fnm.sh
+++ deb/fnm.sh
@@ -7,6 +7,6 @@ else
   chmod +x /tmp/fnm
   /tmp/fnm --install-dir "$HOME/.fnm" --skip-shell
   exec bash -l
-  fnm install "v17.1.0"
-  fnm use "v17.1.0"
+  "$HOME/.fnm/fnm" install "v17.1.0"
+  "$HOME/.fnm/fnm" use "v17.1.0"
 fi
