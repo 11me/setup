38120ebff8364fae5bdcdb57c8149621ac453e26
lime <lime>
2022-01-29 12:21:01 +0300

    fix

--- deb/docker-compose.sh
+++ deb/docker-compose.sh
@@ -3,5 +3,5 @@
 # install docker-compose
 [[ ! -n `command -v docker-compose` ]] && \
  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
- sudo chmod u+x /usr/local/bin/docker-compose \
+ sudo chmod +x /usr/local/bin/docker-compose \
   || echo "Docker-compose already installed, skipping..."
